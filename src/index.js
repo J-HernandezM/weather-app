240f0ecfcc6acea6dfb2fcd27212f0048d7eccac
Juan <juhernandezm@unal.edu.co>
2023-04-10 18:52:16 -0500

    close feature ready and degreess

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const options = {
 const btn = document.querySelector('.searchbtn')
 const input = document.querySelector('.search')
 const mainBox = document.querySelector('main')
+const errorText = document.querySelector('.search__error')
 //CALL
 
 //REQUEST STRUCTURE
@@ -31,34 +32,49 @@ async function fetchData(urlApi){
 //CALLS
 async function call(allAPIS, userInput){
     //VALIDATE THE INPUT
-    let userInputArray = userInput.split(' ')
-    //CHECK IF USER DID 'CITY , COUNTRY'
-    if(userInputArray.includes(',')){
-        userInputArray.splice(userInputArray.indexOf(','), 1)
-        console.log(userInputArray.indexOf(','));
+    let userInputArray 
+    if (userInput.includes(',')){
+        let indexOfComma=userInput.indexOf(',')
+        //IF THERE IS NOT A BLANK SPACE NEX TO THE COMMA, ADD IT
+        if(userInput[indexOfComma+1]!=' '){
+            userInput=userInput.replace(',',', ')
+        }
+        userInputArray= userInput.split(', ')
     }
-    //CHECK IF USER DID 'CITY, COUNTRY'
+    //IF THERE IS NO COMMA, JUST PUT THE TEXT IN AN ARRAY
+    else{
+        userInputArray=[userInput]
+    }
+    //REMOVE SPACES AND COMMAS
     for(let i=0; i<userInputArray.length; i++){
         if(userInputArray[i].includes(',')){
             userInputArray[i]=userInputArray[i].replace(',','')
         }
+        userInputArray[i]=userInputArray[i].trimEnd()
+        userInputArray[i]=userInputArray[i].trimStart()
     }
-    console.log(userInputArray);
-    //ONLY WORKS IF THERE IS TWO WORDS
-    if(userInputArray.length<3){
-        let geoResponse
-        let country = userInputArray[1]
-        let city = userInputArray[0]
-        //IF USER TYPE A COUNTRY
-        if(userInputArray.length==2){
-            const alphaResponse = await fetchData(allAPIS.APIalpha)
-            let alphaCode = getAlphaCode(alphaResponse, country)
-            geoResponse = await fetchData(`${allAPIS.APIgeo}q=${city},${alphaCode}&appid=38dc05b1ec28448bbd96deb0298270be`)
-        }
-        // IF USER ONLY TYPES THE CITY
-        else{
-            geoResponse = await fetchData(`${allAPIS.APIgeo}q=${city}&appid=38dc05b1ec28448bbd96deb0298270be`)
-        }
+
+    //USING THE INPUT
+    let geoResponse
+    let country = userInputArray[1]
+    let city = userInputArray[0]
+    //CALL THE API USING ALPHACODE OR NOT
+    //IF USER TYPES CITY AND COUNTRY
+    if(userInputArray.length==2){
+        const alphaResponse = await fetchData(allAPIS.APIalpha)
+        let alphaCode = getAlphaCode(alphaResponse, country)
+        geoResponse = await fetchData(`${allAPIS.APIgeo}q=${city},${alphaCode}&appid=38dc05b1ec28448bbd96deb0298270be`)
+    }
+    // IF USER ONLY TYPES THE CITY
+    else{
+        geoResponse = await fetchData(`${allAPIS.APIgeo}q=${city}&appid=38dc05b1ec28448bbd96deb0298270be`)
+    }
+    // CHECK IF LOCATION FOUNDED OR NOT
+    if(geoResponse.length==0){
+        errorText.classList.remove('inactive')
+    }
+    else{
+        errorText.classList.add('inactive')
         let geoData = geoResponse[0]
         lat = geoData.lat
         lon = geoData.lon
@@ -67,7 +83,7 @@ async function call(allAPIS, userInput){
         //SAVE STRUCTURE AND NECESSARY MODIFIED 
         let cardTemp = weatherResponse.main.temp.toString().substring(0, 2)
         let cardImageLink = `https://openweathermap.org/img/wn/${weatherResponse.weather[0].icon}@2x.png`
-        let structure = `<figure class="closeBox"><img class="card__close" src="./assets/cerrar.png" alt="close"></figure>
+        let structure = `<figure class="closeBox closeAction"><img class="card__close closeAction" src="./assets/cerrar.png" alt="close"></figure>
         <h2 class="city">${geoData.name}</h2>
         <h3 class="country">${geoData.country}</h3>
         <p class="temp">${cardTemp}</p>
@@ -76,18 +92,18 @@ async function call(allAPIS, userInput){
         </div>
         <p class="card__main">${weatherResponse.weather[0].main}s</p>
         <p class="card__description">${weatherResponse.weather[0].description}</p>`
-
+    
         //CREATE PARENT, ADD STRUCTURE, ADD PARENT TO HTML
         let cardNode = document.createElement('div')
         cardNode.innerHTML=structure
         cardNode.classList.add('card')
         mainBox.append(cardNode)
 
-    }else{
-        console.log('Please use only two words');
+        //ADD THE CLOSE FEATURE
+        
+        
+
     }
-    
-    
 }
 //GET SPECIFIC ALPHA CODE
 function getAlphaCode(allCountries, input){
@@ -107,5 +123,16 @@ input.addEventListener('keyup', (event)=>{
     if(event.keyCode===13){
         let userInput = event.target.parentElement.children[0].value
         call(APIS, userInput)
+    }
+})
+mainBox.addEventListener('click', (event)=>{
+    if(event.target.classList.contains('closeAction')){
+        let currentNode = event.target
+        while(currentNode.parentElement.className!='target'){
+            currentNode=currentNode.parentElement
+        }
+        let parent = currentNode.parentElement
+        let node = currentNode
+        parent.removeChild(node)
     }
 })
